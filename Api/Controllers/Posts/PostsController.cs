3b874468fa66fb29309ab9ed3f3eec4a8904103a
Igor <igor.silva@supermercadosbh.com.br>
2025-04-02 14:58:59 -0300

    Retornar imagem, mudar para o infra e deixar generico

--- Api/Controllers/Posts/PostsController.cs
+++ Api/Controllers/Posts/PostsController.cs
@@ -85,5 +85,38 @@ public class PostsController(IResponseControler responseControler,
         responseControler.AddMessageSuccesso("Post editado com sucesso!");
     }
 
+    [HttpGet, Route("getImagem/{id}")]
+    public async Task<IActionResult> GetImagemAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var model = await repository.GetAsync(id, cancellationToken);
 
+        if (model == null)
+        {
+            responseControler.AddMessageErro("Post não foi encontrado!");
+            return NotFound();
+        }
+
+        if (string.IsNullOrEmpty(model.Image))
+        {
+            responseControler.AddMessageErro("Imagem não foi encontrada!");
+            return NotFound();
+        }
+
+        string base64DataWithPrefix = model.Image;
+
+        string mimeType = "image/png"; // "image/jpeg", "image/gif" e etc;
+
+        // Extrai apenas os dados Base64
+        string base64DataOnly = base64DataWithPrefix.Substring(base64DataWithPrefix.IndexOf(',') + 1);
+
+        try
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64DataOnly);
+            return File(imageBytes, mimeType);
+        }
+        catch (FormatException)
+        {
+            return BadRequest("Formato Base64 inválido (após remoção do prefixo).");
+        }
+    }
 }
