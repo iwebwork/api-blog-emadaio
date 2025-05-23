de99c9b17a8d4849399f021d9efdb51b90648358
iwebwork <iwebwork.adm@gmail.com>
2025-05-23 10:27:28 -0300

    add

--- Api/Controllers/Posts/PostsController.cs
+++ Api/Controllers/Posts/PostsController.cs
@@ -112,7 +112,7 @@ public class PostsController(IResponseControler responseControler,
         {
             return BadRequest("Formato Base64 inválido (após remoção do prefixo).");
         }
-    }00000000000000000000000000000000000000000000000000000000000000000000000000
+    }
 
     [HttpGet, Route("getAnuncio")]
     public string GetAnuncio()
